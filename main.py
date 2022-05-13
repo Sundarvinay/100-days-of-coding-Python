8b9b141e6dd87391f7d0b8df8d5586e22513fdd2
sundar <35004776+Sundarvinay@users.noreply.github.com>
2022-05-13 16:06:58 +0000

    variable

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ two_digit_number = input("Type a two digit number: ")
 # 🚨 Don't change the code above 👆
 
 ####################################
-#Write your code below this line 👇
+#Write your code below this line 
 one_pos = int(two_digit_number)%10
 ten_pos = int(two_digit_number)/10
 print(int(one_pos)+int(ten_pos))
